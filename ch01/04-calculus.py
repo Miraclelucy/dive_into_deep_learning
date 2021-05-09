d325465c68407a39d0dea26a2adfc653473984c2
藤露 <080301087@163.com>
2021-05-09 21:09:47 +0800

    Create 04-calculus.py

---
+++ ch01/04-calculus.py
@@ -0,0 +1,22 @@
+import numpy as np
+from d2l import torch as d2l
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
+def f(x):
+    return 3 * x ** 2 - 4 * x
+
+
+def numerical_lim(f, x, h):
+    return (f(x + h) - f(x)) / h
+
+
+h = 0.1
+for i in range(5):
+    print(f'h={h:.5f}, numerical limit={numerical_lim(f, 1, h):.5f}')
+    h *= 0.1
+
+x = np.arange(0, 3, 0.1)
+d2l.plot(x, [f(x), 2 * x - 3], 'x', 'f(x)', legend=['f(x)', 'Tangent line (x=1)'])
+d2l.plt.show();
