4229b86f20cabcd2d8bc59d4ca96d26c69cd379e
藤露 <080301087@163.com>
2021-05-09 21:07:44 +0800

    Create common.py

---
+++ d2lutil/common.py
@@ -0,0 +1,17 @@
+from d2l import torch as d2l
+
+
+def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
+             legend=None, figsize=(3.5, 2.5)):
+    d2l.set_figsize(figsize)
+    d2l.plt.xlabel(x_label)
+    d2l.plt.ylabel(y_label)
+    d2l.plt.semilogy(x_vals, y_vals)
+    if x2_vals and y2_vals:
+        d2l.plt.semilogy(x2_vals, y2_vals, linestyle=':')
+        d2l.plt.legend(legend)
+    d2l.plt.show()
+
+
+def hello():
+    print("semilogy_HELLO")
