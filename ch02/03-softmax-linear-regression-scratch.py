dfbc1c7fd5b4a13449556f63579f01e2a96cb509
藤露 <080301087@163.com>
2021-05-14 15:38:53 +0800

    Create 03-softmax-linear-regression-scratch.py

---
+++ ch02/03-softmax-linear-regression-scratch.py
@@ -0,0 +1,118 @@
+import sys
+
+import matplotlib.pyplot as plt
+import torch
+from d2l import torch as d2l
+
+sys.path.append('D:\\pythonspace\\d2l\\d2lutil')  # 加入路径，添加目录
+import common
+
+## 读取小批量数据
+batch_size = 256
+train_iter, test_iter = common.load_fashion_mnist(batch_size)
+print(len(train_iter))  # train_iter的长度是235；说明数据被分成了234组大小为256的数据加上最后一组大小不足256的数据
+print('11111111')
+for X, y in train_iter:
+    print(X, y)
+    break; # 尝试打印第一组X, y的形状：torch.Size([256, 1, 28, 28])  torch.Size([256])
+
+
+## 展示部分数据
+def get_fashion_mnist_labels(label):  # @save
+    """返回Fashion-MNIST数据集的文本标签。"""
+    text_labels = [
+        't-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt',
+        'sneaker', 'bag', 'ankle boot']
+    return text_labels[label]
+
+
+def show_fashion_mnist(images, labels):
+    d2l.use_svg_display()
+    # 这里的_表示我们忽略（不使用）的变量
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.view((28, 28)).numpy())
+        f.set_title(get_fashion_mnist_labels(lbl))
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+
+#show_fashion_mnist(train_data[0:10], train_targets[0:10])
+
+# 初始化模型参数
+num_inputs = 784
+num_outputs = 10
+
+W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
+
+# 定义模型
+def softmax(X):
+    X_exp = X.exp()
+    partition = X_exp.sum(dim=1, keepdim=True)
+    return X_exp / partition  # 这里应用了广播机制
+
+
+def net(X):
+    return softmax(torch.matmul(X.reshape(-1, num_inputs), W) + b)
+
+
+# 定义损失函数
+y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+y = torch.LongTensor([0, 2])
+y_hat.gather(1, y.view(-1, 1))
+
+
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat.gather(1, y.view(-1, 1)))
+
+
+# 计算分类准确率
+def accuracy(y_hat, y):
+    return (y_hat.argmax(dim=1) == y).float().mean().item()
+
+
+# 计算这个训练集的准确率
+def evaluate_accuracy(data_iter, net):
+    acc_sum, n = 0.0, 0
+    for X, y in data_iter:
+        acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+        n += y.shape[0]
+    return acc_sum / n
+
+
+num_epochs, lr = 10, 0.1
+
+
+# 本函数已保存在d2lzh包中方便以后使用
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size,
+              params=None, lr=None, optimizer=None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for X, y in train_iter:
+            y_hat = net(X)
+            l = loss(y_hat, y).sum()
+
+            # 梯度清零
+            if params is not None and params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+
+            l.backward()
+            # 执行优化方法
+            if optimizer is not None:
+                optimizer.step()
+            else:
+                d2l.sgd(params, lr, batch_size)
+
+            train_l_sum += l.item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().item()
+            n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, batch_size, [W, b], lr)
