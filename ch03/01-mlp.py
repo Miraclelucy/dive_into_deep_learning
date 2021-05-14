d387fb54e7cb1fa262bffc806f184389d3378d28
藤露 <080301087@163.com>
2021-05-14 15:39:25 +0800

    Update 01-mlp.py

--- ch03/01-mlp.py
+++ ch03/01-mlp.py
@@ -1,1 +1,35 @@
 
+import os
+
+import torch
+from d2l import torch as d2l
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
+y = torch.relu(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'relu(x)', figsize=(5, 2.5))
+d2l.plt.show();
+
+y.sum().backward()
+d2l.plot(x.detach(), x.grad, 'x', 'grad of relu', figsize=(5, 2.5))
+d2l.plt.show();
+
+y = torch.sigmoid(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'sigmoid(x)', figsize=(5, 2.5))
+d2l.plt.show();
+
+x.grad.zero_()
+y.sum().backward()
+d2l.plot(x.detach(), x.grad, 'x', 'grad of sigmoid', figsize=(5, 2.5))
+d2l.plt.show();
+
+y = torch.tanh(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'tanh(x)', figsize=(5, 2.5))
+d2l.plt.show();
+
+x.grad.zero_()
+y.sum().backward()
+d2l.plot(x.detach(), x.grad, 'x', 'grad of tanh', figsize=(5, 2.5))
+d2l.plt.show();
+
